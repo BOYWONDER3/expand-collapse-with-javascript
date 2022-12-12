9740f4ece6b1714f1bca4840c89e72b7664fc4e9
boywonder3 <opeyemiisaacboywonder@gmail.com>
2022-12-13 00:31:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('click', e => {
+    if(!e.target.matches('.expand-button')) return
+
+    const card = e.target.closest('.card')
+    const cardBody = card.querySelector('.card-body')
+
+    cardBody.classList.toggle('show')
+    if (e.target.innerText === 'Expand') {
+        e.target.innerText = 'collapse'
+    } else {
+        e.target.innerText = 'Expand'
+    }
+
+})
+
+console.log('vuch')
